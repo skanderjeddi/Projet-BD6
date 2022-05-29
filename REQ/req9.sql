6cfd93a7857f88893e308db3dca904f639e79bce
Skander Jeddi <skander.jeddi2@gmail.com>
2022-05-29 23:54:25 +0200

    Reqs

--- REQ/req9.sql
+++ REQ/req9.sql
@@ -1,1 +1,9 @@
--- Quels sont les navires les plus chargés à être passé par chaque port ?
+-- Cette requête récupère la liste ordonnée des navires ayant transporté le plus de marchandise
+SELECT N.navire_id, SUM(cargaison_quantite_produit * produit_volume_kg) AS charge
+FROM navire N JOIN voyage V ON N.navire_id = V.voyage_navire_id
+JOIN port P on P.port_id = V.voyage_port_depart OR P.port_id = V.voyage_port_arrivee
+JOIN etape E ON E.etape_port_id = P.port_id
+JOIN cargaison C ON C.cargaison_etape_id = E.etape_id
+JOIN produit P2 ON P2.produit_id = C.cargaison_produit_id
+GROUP BY N.navire_id
+ORDER BY charge DESC;
